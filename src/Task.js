0a5abe1d4e7785bfbb1b06fdfe75ed6db4fee5da
Erwin Debusschere <erwincdl@gmail.com>
2018-03-15 16:54:17 -0300

    Arreglo margenes de la notificación y le pongo color a los labels de la lista

--- src/Task.js
+++ src/Task.js
@@ -5,10 +5,10 @@ class Task extends Component {
   render() {
     return (
       <div onClick={this.taskClicked}>
-        <div style={{ border: '1px solid white', margin: 10, padding: 5, borderRadius: 10 }}>
-          Title: {this.props.title}
+        <div style={{ border: '1px solid white', margin: 5, padding: 15, borderRadius: 10 }}>
+          <b>Title:</b> {this.props.title}
           <br/>
-          Description: {this.props.description}
+          <b>Description:</b> {this.props.description}
         </div>
       </div>
     );
